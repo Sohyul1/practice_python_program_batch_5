a825637b24d193e624bf9a2455c1dc2c18ecfcf3
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:01:17 +0800

    input statement and used .split

--- count_word_in_statement.py
+++ count_word_in_statement.py
@@ -1,5 +1,9 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
 # Ask for user input
+statement = input("Enter a complete statement: ")
+
 # Use split to split the words
+statement.split()
 # Count the words usinng len
 # Print the statement
