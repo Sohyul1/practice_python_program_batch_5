7a86eb66ec714a3412c245a2ae566082703585f2
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:35:45 +0800

    input fullname, used .lower and made a new variable name, then print

--- print_name_in_snake_casing.py
+++ print_name_in_snake_casing.py
@@ -1,4 +1,10 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
 # Ask for user input
+full_name = input("Enter your name in incorrect casing: ")
+
 # Convert it to snake casing (probably using .lower and .replace)
+full_name_snake_case = full_name.lower()
+
 # Print the name
+print(full_name_snake_case)
