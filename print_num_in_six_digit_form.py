2eef5d64ceb819ee170ac8591692929855924622
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:25:00 +0800

    used zfill but it's now working

--- print_num_in_six_digit_form.py
+++ print_num_in_six_digit_form.py
@@ -1,3 +1,7 @@
 # Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
 # Ask for user input
+num = int(input("Enter a number from 0-100: "))
+
 # Add zeros and print
+print(num.zfill())
