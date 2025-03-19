ea479acbb366df096bf3bf1b7c48d8797046221f
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:11:19 +0800

    pseudocode

---
+++ remove_spaces_at_beginning.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+# Ask for user input
+# Remove the spaces
+# Print the output
