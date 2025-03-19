d525f5f7ea87ca1ed263fdcfba7e434f366b4215
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:38:20 +0800

    input name and used .lower then print

--- print_name_in_lower_case.py
+++ print_name_in_lower_case.py
@@ -1,4 +1,7 @@
 # Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+
 # Ask for user input
+name = input("Enter  your fullname: ")
+
 # Convert in lowercase form 
-# Print the name
+print(name.lower())    # Print the name
