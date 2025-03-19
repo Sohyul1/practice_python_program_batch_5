849ab28184888a5aed0ef49e2fb1160f534986f9
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:39:33 +0800

    edited the input message

--- remove_spaces_at_beginning.py
+++ remove_spaces_at_beginning.py
@@ -1,7 +1,7 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # Ask for user input
-name = input("Enter your name(with spaces at the beginning): ")
+name = input("Enter your name with spaces at the beginning: ")
 
 # Remove the spaces
 print(name.lstrip()) # Print the output
