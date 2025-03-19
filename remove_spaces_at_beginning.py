4d3c15e080e971f2f248d61a16d5d2232a9aab38
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:23:03 +0800

    changed to lstrip to be more specific

--- remove_spaces_at_beginning.py
+++ remove_spaces_at_beginning.py
@@ -1,8 +1,8 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # Ask for user input
-name = input("Enter tour name(with spaces at the beginning): ")
+name = input("Enter your name(with spaces at the beginning): ")
 
 # Remove the spaces
-print(name.strip()) # Print the output
+print(name.lstrip()) # Print the output
 
