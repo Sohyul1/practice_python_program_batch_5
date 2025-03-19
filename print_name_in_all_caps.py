76929f9a463cf4acc2db815c52cadc9b070d9603
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:29:48 +0800

    pseudocode

---
+++ print_name_in_all_caps.py
@@ -0,0 +1,4 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Ask for user input
+# Convert the input in capslock
+# Print the output
