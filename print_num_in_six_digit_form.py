c46ff34ba31ca00835735177c0073b831a10199f
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:27:53 +0800

    removed the int and it worked

--- print_num_in_six_digit_form.py
+++ print_num_in_six_digit_form.py
@@ -1,7 +1,7 @@
 # Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # Ask for user input
-num = int(input("Enter a number from 0-100: "))
+num = input("Enter a number from 0-1000: ")
 
 # Add zeros and print
-print(num.zfill())
+print(num.zfill(6))
