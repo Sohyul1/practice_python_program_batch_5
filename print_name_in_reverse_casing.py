c702a9d02248718937ef22ff537a885c39f47c96
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:48:47 +0800

    changed the variable name

--- print_name_in_reverse_casing.py
+++ print_name_in_reverse_casing.py
@@ -1,7 +1,7 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 # Ask for user input
-name = input("Enter your name (in incorrect spacing): ")
+full_name = input("Enter your name in incorrect spacing: ")
 
 # Convert to reverse case
-print(name.swapcase())    # Print the name 
+print(full_name.swapcase())    # Print the name 
