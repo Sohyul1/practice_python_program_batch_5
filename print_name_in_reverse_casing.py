9bacea6198cfcc775763ff5aa52c78371e9d02f2
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:45:57 +0800

    pseudocode

---
+++ print_name_in_reverse_casing.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Ask for user input
+# Convert to reverse case
+# Pritn the name 
