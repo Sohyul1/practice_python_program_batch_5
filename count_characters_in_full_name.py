3f39bf3199e58fd896e002552c507c4d4b975950
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:13:46 +0800

    fix the typy error

--- count_characters_in_full_name.py
+++ count_characters_in_full_name.py
@@ -1,7 +1,7 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # Ask for user input
-full_name = input("Enter yout fullname: ")
+full_name = input("Enter your fullname: ")
 
 # Count the characters
 print(len(full_name))  # Print the number of Characters
