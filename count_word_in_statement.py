646aee9ed703d564be390d45581c4cbce2755a1c
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:59:07 +0800

    pseudocode

---
+++ count_word_in_statement.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+# Ask for user input
+# Use split to split the words
+# Count the words usinng len
+# Print the statement
