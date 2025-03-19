2da1ae986e1df000510bebd112ed9a6cbd8556e8
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:08:54 +0800

    pseudocode

---
+++ count_characters_in_full_name.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+# Ask for user input
+# Count the characters
+# Print the number of Characters
