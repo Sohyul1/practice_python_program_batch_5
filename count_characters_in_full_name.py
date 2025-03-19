db08c955a6f90d04b4f8816728cf73f35f6c2376
Aeron <dequitoaeron353@gmail.com>
2025-03-19 16:12:53 +0800

    used len to count since spaces are counted, then print

--- count_characters_in_full_name.py
+++ count_characters_in_full_name.py
@@ -1,4 +1,7 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
 # Ask for user input
+full_name = input("Enter yout fullname: ")
+
 # Count the characters
-# Print the number of Characters
+print(len(full_name))  # Print the number of Characters
