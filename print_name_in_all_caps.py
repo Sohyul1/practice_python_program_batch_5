61aca6fd75021ca447549431d8074b31128530ea
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:33:06 +0800

    input fullname and print (used  .upper())

--- print_name_in_all_caps.py
+++ print_name_in_all_caps.py
@@ -1,4 +1,7 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
 # Ask for user input
+name = input("Enter your fullname: ")
+
 # Convert the input in capslock
-# Print the output
+print(name.upper()) # Print the output
