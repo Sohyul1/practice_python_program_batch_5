5b26ec00889054e536858ec885e08d4040442f9b
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:54:04 +0800

    input name, use swapcase and print

--- print_name_in_reverse_casing.py
+++ print_name_in_reverse_casing.py
@@ -1,4 +1,7 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
 # Ask for user input
+name = input("Enter your name (in incorrect spacing): ")
+
 # Convert to reverse case
-# Pritn the name 
+print(name.swapcase())    # Print the name 
