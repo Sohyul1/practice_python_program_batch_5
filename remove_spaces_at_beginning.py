e9e884444fde301460c625ce5153d7fd12faca3b
Aeron <dequitoaeron353@gmail.com>
2025-03-19 15:17:01 +0800

    input name and print part

--- remove_spaces_at_beginning.py
+++ remove_spaces_at_beginning.py
@@ -1,4 +1,8 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
 # Ask for user input
+name = input("Enter tour name(with spaces at the beginning): ")
+
 # Remove the spaces
-# Print the output
+print(name.strip()) # Print the output
+
